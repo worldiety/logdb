4116c5cb6f77fa76dd63c0d3fa8fe2fe2ec2b64c
Torben Schinke <torben.schinke@worldiety.de>
2020-06-25 18:53:21 +0200

    added experimental AutoInt and AutoFloat write detection

--- objecthelper.go
+++ objecthelper.go
@@ -1,5 +1,50 @@
 package logdb
 
+import (
+	"github.com/worldiety/ioutil"
+	"math"
+)
+
+// AddAutoInt inspects the value and chooses automatically between 1/2/4/8 bytes
+func (d *Object) AddAutoInt(name uint32, v int64) {
+	if v >= ioutil.MinInt8 && v <= ioutil.MaxUint8 {
+		d.AddUint8(name, uint8(v))
+		return
+	}
+
+	if v >= ioutil.MinInt16 && v <= ioutil.MaxUint16 {
+		d.AddUint16(name, uint16(v))
+		return
+	}
+
+	if v >= ioutil.MinInt32 && v <= ioutil.MaxUint32 {
+		d.AddUint32(name, uint32(v))
+		return
+	}
+
+	d.AddInt64(name, v)
+}
+
+// AddAutoFloat inspects the value and chooses automatically between int 1/2/4/8 and float32/float64.
+func (d *Object) AddAutoFloat(name uint32, v float64) {
+	const epsilon = 1e-9
+
+	// looks like an int?
+	if _, frac := math.Modf(math.Abs(v)); frac < epsilon || frac > 1.0-epsilon {
+		d.AddAutoInt(name, int64(v))
+		return
+	}
+
+	// looks like it fits into float32?
+	tmp := v * 100
+	if _, frac := math.Modf(math.Abs(tmp)); frac < epsilon || frac > 1.0-epsilon {
+		d.AddFloat32(name, float32(v))
+		return
+	}
+
+	d.AddFloat64(name, v)
+}
+
 func (d *Object) AddFloat32(name uint32, v float32) {
 	count := d.FieldCount()
 	d.buf.Pos = int(d.Size())
@@ -18,6 +63,10 @@ func (d *Object) AddFloat64(name uint32, v float64) {
 	d.buf.WriteFloat64(v)
 	d.setSize(uint32(d.buf.Pos))
 	d.setFieldCount(count + 1)
+}
+
+func (d *Object) AddInt32(name uint32, v int32) {
+	d.AddUint32(name, uint32(v))
 }
 
 func (d *Object) AddUint32(name uint32, v uint32) {
